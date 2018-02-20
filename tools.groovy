b0072688a21dddba52f1b50169b0e2b58d10d01e
robertschimpfle <robert.schimpfle@comquent.de>
2018-02-20 17:44:23 +0100

    Update tools.groovy
    add mvn 3.5.2 as tool

--- tools.groovy
+++ tools.groovy
@@ -1,1 +1,18 @@
-// install tools
+import jenkins.*;
+import jenkins.model.*;
+import hudson.*;
+import hudson.model.*;
+ 
+mavenName = "MVN 3.5"
+mavenVersion = "3.5.2"
+ 
+// Grab the Maven "task" (which is the plugin handle).
+mavenPlugin = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0]
+ 
+// Add an installation.
+newMavenInstall = new hudson.tasks.Maven.MavenInstallation('maven-3', null,
+    [new hudson.tools.InstallSourceProperty([new hudson.tasks.Maven.MavenInstaller(mavenVersion)])]
+)
+
+mavenPlugin.installations += newMavenInstall
+mavenPlugin.save()
